ef484bb16c1fe2a3cfd071dcc86652779f6cf3f3
Dancsok Zoltán <58068447+Strogg-byte@users.noreply.github.com>
2022-10-20 21:10:18 +0200

    Update smartwatch_v2.ino

--- smartwatch_v2.ino
+++ smartwatch_v2.ino
@@ -1,212 +1,207 @@
-/*
-Smartwatch V2 by Zoltan "strogg" Dancsok dancsok.zoltan@outlook.com
-
-Hardware items
-- ssd1306 oled screen i2c
-- gy-91 multisensor module i2c
-- heartbeat pulse sensor
-- tp battery charger.
-- piezzo 
-- vibro
-- usb slot
-
-Function list:
-- read temp,humidiy,altitude, accelometer, compass, pulse,bloodO2
-- select screens: - welcome screen = boot screen 2 sec.
-                  - Default screen: uptime, bt status, compass circle, gyro cyrcle, watch (from BT), notifications
-                  - Sensor screen: alt,temp,hum,gyro,etc... 
-                  - Health: bpm data, pulse animation
-                  - ALL  bt,mail,sms,call, status, sight, write bpm anim, compass+gyro cycle
-                  - Debug: write all sensor data
-                  - External mode serial mode: usb serial input data
-
-Eqv: 
-- temp 30°+accel >3  = water warning
-- temp > 30° = high temperature warn
-- temp > 35° = HEAT warn.
-- hum  >70%  = high hum. warn. 
-- hum <5 = cold warn.
-- hum < 0 = ice warn.
-- bpm > 100 = High pulse warn.
-- bpm > 120 = High pulse Danger! Call 112?
-- bpm < 50  = Low pulse Danger! Call 112?
-
-- Hour > 11 & <12 = eat warn.
-- Hour > 9 & <10  = eat warn.
-- Hour > 18 & <20  = eat warn.
-
-
-*/
-
-#include <SPI.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <dht.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "strings.h"
 #include <SoftwareSerial.h>
 
-SoftwareSerial usb_in(3,4);
 
-const int hbeat = 0; // A0 pulse pin
-int t_back     = A1;
-int t_next     = A2;
-int brain      = A3;
-int piezo      = 2;
+dht DHT;
+#define DHT11_PIN 10
+uint32_t delayMS;
 
-int modes      = 0; // screen modes
-int screens    = 0; // 0-4 
+SoftwareSerial BTserial(2, 3); //Connect HC-06, RX, TX
+char c=' ';
+String data; 
+int temp = 0;
+int hum  = 0;
+
+int analogInput = 0;
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 100000.0; // resistance of R1 (100K)
+float R2 = 10000.0; // resistance of R2 (10K)
+int value = 0;
+int a[5] = {};
+
+int screen = 1;
+
+Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
+unsigned long delayTime;
+
+
 int timer      = 0; // timer value
-int hours      = 0; 
-int minutes    = 0;
-int secs       = 0; 
-int stepper    = 0; // default main timer
-int bt_status  = 0;   // Bt connect status     
-int on_off     = 0; // on off status
-int pulse      = 0; // bpm data
-int warn_time  = 3000; // warning strings screen time in ms
-int piezo_time = 1000; // piezzo sound time in ms
 int headache   = 0;   // headache vibro mode status 0,1
- 
-int gir_x      = 0;
-int git_y      = 0;
-int gir_z      = 0;
-
-int acc_x      = 0;
-int acc_y      = 0;
-int acc_z      = 0;
-
-int comp_x     = 0;
-int comp_y     = 0;
-int comp_z     = 0;
-
-float temp     = 0.00;
-float hum      = 0.00;
-float altitude = 0.00;
-
-String bt_msg  = "";
-String lastValue;
-
-char blueToothVal;  
-
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define GREEN 0x07E0
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  usb_in.begin(9600);
-  pinMode(piezzo,OUTPUT);
+  Serial.println("KIROSHI OPTICALS Opti-Flash Mk1. Booting");
+pinMode(analogInput, INPUT);
+
+  BTserial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
   
-  
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
  
-    
-}
-
- display.mirrorScreen();
- display.setBrightness(252);
-
 welcome();
+//delay(3000);
+//main_screen();
 }
 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void loop() { 
 timer++;
+if(timer==3000){timer=0;}
 
+int chk = DHT.read11(DHT11_PIN);
+if(DHT.temperature > -50){temp = DHT.temperature;}
+//temp = DHT.temperature ;
+if(DHT.humidity > -50){hum  = DHT.humidity;}
+
+vin = 0.0;
+
+value = analogRead(analogInput);
+  vout = (value * 5) / 1024.0;
+  vin = vout / (R2 / (R1 + R2));
+  if (vin < 0.09)
+  {
+    vin = 0.0;
+  }
+if(vin  < 1 and !BTserial.available() and !Serial.available() and screen == 2){ main_screen(); screen =1; }
+
+if(vin > 1 and !BTserial.available() and !Serial.available()){ 
+  screen = 2;
+  vin = vin + 0.3;
+  
+        display.setTextSize(2);
+        display.clearDisplay();
+        display.setCursor(25,20); 
+        display.print(String(vin)+ " V");
+        display.display();
+        Serial.println(vin);
+        BTserial.println(vin); 
+          
+  }
+
+ if (BTserial.available()){  
+        c = BTserial.read();
+        Serial.write(c);
+      if (c != -1) {
+      data += c; 
+      if (c == '\n') { 
+        display.setTextSize(1);
+        display.setCursor(1,1);
+        display.clearDisplay();
+        display.print(data); 
+        display.display();
+        
+        delay(5000);
+        data = ""; 
+        main_screen();
+        
+          }
+       }
+        
+    }
+ 
+   
+    if (Serial.available()){
+        c =  Serial.read();
+        BTserial.write(c); 
+        Serial.write(c); 
+        if (c != -1) {
+      data += c; 
+      if (c == '\n') { 
+        display.setTextSize(1);   
+        display.setCursor(1,1);
+        display.clearDisplay();
+        display.print(data); 
+        display.display();
+        delay(5000);
+        data = ""; 
+        main_screen();
+        
+          }
+       }
+        
+        
+    }
+
+
+
+//####################### main screen
+ if(timer ==500){
+  main_screen();
+ }
+
+//*****************
+//###########################################
 }
 
-//############################## DISPLAYS ####################################
 
-void display_sight(void){
-  display.clearDisplay(); //128x64
-//*********animations
-if(stepper >=1){ display.fillCircle(2,43, 1, WHITE); }
-if(stepper >=2){ display.fillCircle(2,33, 1, WHITE); }
-if(stepper >=3){ display.fillCircle(2,23, 1, WHITE); }
-if(stepper >=4){ display.fillCircle(2,13, 1, WHITE); }
-//**********************
+void welcome(){
+ display.clearDisplay();
+   display.setCursor(25,0);
+   display.println("KIROSHI");
+   display.setCursor(25,10);
+   display.println("OPTICALS");
+   display.setCursor(25,20);
+   display.println("1990-2022");
+   display.setCursor(12,30);
+   display.println("Opti-Flash Mk1."); 
+   display.setCursor(25,40);
+   display.println("Hud Edition");
+   display.display(); 
+}
+
+void main_screen(){
+ display.setTextSize(1);
+ display.clearDisplay();
+ display.setCursor(20,0);
+ display.print(temp);
+display.setCursor(35,0);
+display.print("C");
+ 
+ display.setCursor(90,0);
+ display.print(hum);
+ 
+ display.setCursor(105,0);
+  display.print("%");
+
+//############### figyelmeztetesek
+if(temp >=30){display.setCursor(40,0); display.print("!");}
+if(hum >=70){display.setCursor(85,0); display.print("!");}
+
+
+if(temp <= 0){display.setCursor(45,0); display.print("!");}
+
+//#################################
   
-  display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(WHITE);        // Draw white text
-  display.setCursor(1,0);             // Start at top-left corner
-
-  display.fillCircle(78, 32, 1, WHITE);
-  display.fillCircle(92, 32, 1, WHITE);
-  display.fillCircle(106, 32, 1, WHITE);
-  display.fillCircle(display.width() / 2, display.height() / 2, 2, WHITE); // 64x32
-  display.fillCircle(50, 32, 1, WHITE);
-  display.fillCircle(36, 32, 1, WHITE); 
-  display.fillCircle(22, 32, 1, WHITE);
-
-  display.fillCircle(64,  6, 1, WHITE);
-  display.fillCircle(64, 18, 1, WHITE);
-  display.fillCircle(64, 46, 1, WHITE);
-
+  display.drawPixel(78, 32, WHITE);
+  display.drawPixel(92, 32,  WHITE);
+  display.drawPixel(106, 32, WHITE);
+  display.drawPixel(64,32, WHITE); // 64x32
+  display.drawPixel(50, 32,  WHITE);
+  display.drawPixel(36, 32,  WHITE); 
+  display.drawPixel(22, 32, WHITE);
+  display.drawPixel(64,  6,  WHITE);
+  display.drawPixel(64, 18, WHITE);
+  display.drawPixel(64, 46, WHITE);
    display.drawLine(0, 0, 0, 10, WHITE); // |
    display.drawLine(0, 0, 10, 0, WHITE); //-
    display.setCursor(15,0);
- 
- /* if(bt_status == 1){
-  display.println(String(message_str[1])); 
-  } */
-
-//Serial.println(String(bt_status));
-   
    display.drawLine(117, 0, 127, 0, WHITE); //-
-
    display.drawLine(0, 53, 0, 63, WHITE); // |
    display.drawLine(0, 63, 10, 63, WHITE); //- 
-  
    display.drawLine(117, 63, 127, 63, WHITE); // |
    display.drawLine(127, 0, 127, 63, WHITE); // |
-
-    //display.drawLine(124, 10, 127, 10, WHITE); 
-    display.setCursor(100,1);
-    display.println("05");
-
     display.drawLine(124, 20, 127, 20, WHITE); 
-    display.setCursor(100,18);
-    display.println("1");
-    
     display.drawLine(124, 40, 127, 40, WHITE); 
-    display.setCursor(100,38);
-    display.println("3"); 
-display.setCursor(20,55);
-
-if(bt_msg == ""){
-display.println("Mission:"+String(hours)+":"+String(minutes)+":"+String(secs));
-}  else {
-display.println(bt_msg);  
+  display.setCursor(20,55);
+    display.display();
+ 
 }
-
-    display.display(); 
-    delay(1);
-//*****************
-  
-} // Sight END
-
-void welcome(void) {
-   display.clearDisplay();
-   display.setTextColor(WHITE);
-   display.setCursor(25,5);
-   display.setTextSize(1);
-   display.println("SMART GLASS");
-   display.setCursor(25,20);
-   display.setTextColor(WHITE);
-   display.println("v.2.0.1");
-   display.setCursor(25,30);
-   display.println("by Strogg");
-   display.setCursor(12,40);
-   display.println("dancsok.zoltan@outlook.com"); 
-   display.display(); 
-   delay(5000);
- }
